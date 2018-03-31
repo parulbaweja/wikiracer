1db6feaa22b9c765e85049ea1be9c99cd1661420
Parul Baweja <parulbaweja8@gmail.com>
2018-03-31 14:37:00 -0700

    'Working with coroutines, need to stop workers after finding path'

--- coros.py
+++ coros.py
@@ -2,25 +2,18 @@ import aiohttp
 from wiki import wiki_request
 import asyncio
 
-topics = ['Python', 'Albert Einstein']
-
 class WikiFetch(object):
 
     def __init__(self):
         self.to_fetch = asyncio.Queue()
 
-    async def worker(i):
+    async def worker(self):
         async with aiohttp.ClientSession() as session:
             while True:
-                titles = await wiki_request(session, await to_fetch.get())
-                print(titles)
+                title_to_fetch, cb, depth = await self.to_fetch.get()
+                resp = await wiki_request(session, title_to_fetch)
+                await cb(title_to_fetch, resp, depth)
 
-    async def producer(topic):
-        await to_fetch.put(topic)
+    async def producer(self, topic, cb, depth):
+        await self.to_fetch.put((topic, cb, depth))
 
-    def main():
-        coros = [worker(i) for i in range(2)]
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(asyncio.gather(producer(), *coros))
-
-main()
--- main.py
+++ main.py
@@ -1,56 +1,65 @@
 from queue import Queue
 from wiki import wiki_request
 import coros
-# import asyncio
+import asyncio
 # import aiohttp
 
 class WikiGraph(object):
     def __init__(self):
         self.graph = {}
-        self.to_visit = Queue()
+        self.to_visit = asyncio.Queue()
+        self.fetcher = coros.WikiFetch()
+        self.came_from = {}
 
-    def shortest_path(self, start, end):
+    async def shortest_path(self, start, end):
         if start == end:
             print('The start and end are the same')
             return
 
-        came_from = {
-            start: None,
-        }
+        self.came_from[start] = None
 
-        self.to_visit.put((start, 0))
+        await self.to_visit.put((start, 0))
 
-        while self.to_visit:
-            cur, depth = self.to_visit.get()
+        while True:
+            cur, depth = await self.to_visit.get()
+
+            if cur == end:
+                return self.find_path(self.came_from, cur)
+
             if depth == 10:
-                break
+                return
 
             if cur not in self.graph:
-                # enqueue for worker
-                self.graph[cur] = wiki_request(cur)
-            sublinks = self.graph[cur]
-
-            for sublink in sublinks:
-                if sublink in came_from:
-                    continue
-                came_from[sublink] = cur
-                if sublink == end:
-                    return self.find_path(came_from, sublink)
-                self.to_visit.put((sublink, depth + 1))
+                await self.fetcher.producer(cur, self.queue_links, depth)
+                continue
+            else:
+                await self.queue_links(cur, self.graph[cur])
 
         print('No path found')
 
     def find_path(self, parents, dest):
-
         path = [dest]
         while parents[dest] is not None:
             path.append(parents[dest])
             dest = parents[dest]
 
         path.reverse()
+        print(path)
         return path
 
+    async def queue_links(self, cur, resp, depth):
+        self.graph[cur] = resp
+        for link in resp:
+            if link in self.came_from:
+                continue
+            self.came_from[link] = cur
+            await self.to_visit.put((link, depth + 1))
 
-# p = WikiGraph('Python', 'Total Film')
-# s = p.shortest_path()
-# print(s)
+
+p = WikiGraph()
+coros = [p.fetcher.worker() for i in range(10)]
+loop = asyncio.get_event_loop()
+loop.run_until_complete(asyncio.gather(
+    p.shortest_path('Python', 'Total Film'),
+    *coros
+))
